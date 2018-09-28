d9ba1d1fde40de2cc1d03baf0e3f5bcede001e89
Saikat Das <saikatdas0790@gmail.com>
2018-09-28 13:38:06 +0530

    Add componentDidMount illustration and finish code

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ class App extends Component {
     characters: []
   };
 
+  // // Code is invoked after the component is mounted/inserted into the DOM tree.
+  // componentDidMount() {
+  //   const url =
+  //     "https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*";
+
+  //   fetch(url)
+  //     .then(result => result.json())
+  //     .then(result => {
+  //       this.setState({ data: result });
+  //     });
+  // }
+
   removeCharacter = index => {
     const { characters } = this.state;
 
@@ -22,6 +34,12 @@ class App extends Component {
   };
 
   render() {
+    // const { data } = this.state;
+    // const result = data.map((entry, index) => {
+    //   return <li key={index}>{entry}</li>;
+    // });
+
+    // return <ul>{result}</ul>;
     const { characters } = this.state;
 
     return (
